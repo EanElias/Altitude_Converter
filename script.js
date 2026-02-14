43f3c7634ea592f1bb34d96011c59d18d20312a1
EanElias <eanamgad@gmail.com>
2026-02-13 17:45:57 -0700

    Add distance selection for altitude time conversion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let distanceSelect=document.getElementById("Distance")
 let timeInput=document.getElementById("initialTime");
 let altitudeInput=document.getElementById("altitude");
 let button=document.getElementById("convertButton");
@@ -6,10 +7,11 @@ let result=document.getElementById("result")
 
 button.addEventListener("click",function() {
     console.log("Button clicked")
+    let distanceValue=distanceSelect.value;
     let timeValue=timeInput.value;
     let altitudeValue=altitudeInput.value;
     console.log(timeValue,altitudeValue);
-    let altitudeMeters = altitudeValue*0.305;
+    let altitudeMeters = altitudeValue*0.3048;
     function convertTimeToSeconds(mmSsString) {
         const[minutes, seconds]=mmSsString.split(':');
         const totalSeconds = (+minutes)*60 +(+seconds);
@@ -17,7 +19,16 @@ button.addEventListener("click",function() {
     }
     let totalSeconds= convertTimeToSeconds(timeValue);
     function altitudeEquation(totalSeconds, altitude){
-       const k=0.018;
+       const kValues={
+        "800m":0.010,
+        "1600m": 0.018,
+        "3200m": 0.022,
+        "5000m": 0.025,
+        "10000m": 0.028,
+        "Half-Marathon": 0.032,
+        "Marathon": 0.035,
+       };
+       let k =kValues[distanceValue];
        const percent_loss =k*(altitude/1000);
        const seeLevelTime= totalSeconds*(1-percent_loss);
        return seeLevelTime;
